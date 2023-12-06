183766e34268160a1b696f368ea7806f04b4d3b9
User User <ganesh.nanduru@gmail.com>
2023-12-06 00:58:01 -0500

    day 1 pt 2

--- day_1_trebuchet.cpp
+++ day_1_trebuchet.cpp
@@ -6,31 +6,87 @@
 
 using namespace std;
 
+string digits[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
 int get_first_digit(string line){
-    for (int i=0; i<line.length(); i++){
-        if (isdigit(line[i])){
-            return int(line[i]-'0');
+    int min_ind = line.length();
+    int num = 0;
+    for (int i=0; i<size(digits); i++){
+        string digit = digits[i];
+        int ind = line.find(digit);
+        if (ind != string::npos){
+            if (ind < min_ind){
+                min_ind = ind;
+                num = i+1;
+            }
         }
     }
-    throw std::invalid_argument( "digit not found in string" );
+
+    int to_return = -1;
+    for (int i=0; i<line.length(); i++){
+        if (isdigit(line[i])){
+            if (i > min_ind) { 
+                to_return = num;
+            }
+            else{
+                
+                to_return = int(line[i]-'0');
+                
+            }
+            break;
+        }
+    }
+    if (to_return == -1) 
+        to_return = num;
+    
+    
+    
+    return to_return;
 }
 
 int get_last_digit(string line){
-    for (int i=line.length()-1; i>=0; i--){
-        if (isdigit(line[i])){
-            return int(line[i]-'0');
+    int max_ind = -1;
+    int num = 0;
+    for (int i=0; i<size(digits); i++){
+        string digit = digits[i];
+        int ind = line.find(digit);
+        if (ind != string::npos){
+            if (ind > max_ind){
+                max_ind = ind;
+                num = i+1;
+            }
         }
     }
-    throw std::invalid_argument( "digit not found in string" );
+    int to_return = -1;
+    for (int i=line.length()-1; i>=0; i--){
+        if (isdigit(line[i])){
+            if (i < max_ind) { 
+                to_return = num;
+            }
+            else {
+                to_return = int(line[i]-'0');
+            }
+            break;
+        }
+    }
+    if (to_return == -1) to_return = num;
+    
+    return to_return;
 }
 
 int main() {
+    
     int sum = 0;
+    int num;
     string line;
+    
     while (getline(cin, line))
     {
-        sum += 10*get_first_digit(line) + get_last_digit(line);
+        num = 10*get_first_digit(line) + get_last_digit(line);
+        // cout << line << " " <<num << "\n";
+        sum += num;
     }
+    
     cout << sum;
 }
 
